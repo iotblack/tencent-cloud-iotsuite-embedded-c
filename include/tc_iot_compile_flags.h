0d86a8c0b71f5a898201908f633814c306b817f0
huxinbang <huxinbang@gmail.com>
2018-07-31 19:29:30 +0800

    merge

--- include/tc_iot_compile_flags.h
+++ include/tc_iot_compile_flags.h
@@ -2,12 +2,12 @@
 #define TC_IOT_COMPILE_FLAGS_H
 
 #define ENABLE_MQTT
-#define ENABLE_OTA
+// #define ENABLE_OTA
 #define ENABLE_DATA_TEMPLATE
-
-#define ENABLE_COAP
-
-#define ENABLE_TLS
+//
+// #define ENABLE_COAP
+//
+// #define ENABLE_TLS
 /* #undef ENABLE_DTLS */
 
 #define ENABLE_STACK_TRACE_LOG
